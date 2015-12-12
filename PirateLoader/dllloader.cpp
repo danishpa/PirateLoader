65225c9459ad72f3dc1685d8b1b288449983babc
Danish <danishpa@gmail.com>
2015-12-12 18:11:55 +0200

    Imperfect implementation of dll search path

--- PirateLoader/dllloader.cpp
+++ PirateLoader/dllloader.cpp
@@ -27,24 +27,37 @@ namespace dllloader {
 	string DllLoader::search_dll_path(string name) {
 		// !TODO: Try to imitate dll searching:
 		// https://msdn.microsoft.com/en-us/library/windows/desktop/ms682586(v=vs.85).aspx#standard_search_order_for_desktop_applications
-		//	- The directory from which the application loaded.
-		//	- The current directory.
-		//	- The system directory.Use the GetSystemDirectory function to get the path of this directory.
-		//	- The 16 - bit system directory.There is no function that obtains the path of this directory, but it is searched.
-		//	- The Windows directory.Use the GetWindowsDirectory function to get the path of this directory.
-		//	- The directories that are listed in the PATH environment variable.Note that this does not include the per - application path specified by the App Paths registry key.The App Paths key is not used when computing the DLL search path.
+		//	[ ] - The directory from which the application loaded.
+		//	[V] - The current directory.
+		//	[V] - The system directory.Use the GetSystemDirectory function to get the path of this directory.
+		//	[ ] - The 16 - bit system directory.There is no function that obtains the path of this directory, but it is searched.
+		//	[V] - The Windows directory.Use the GetWindowsDirectory function to get the path of this directory.
+		//	[?] - The directories that are listed in the PATH environment variable.Note that this does not include the per - application path specified by the App Paths registry key.The App Paths key is not used when computing the DLL search path.
 
-		// Currently just using PathFindOnPath
+		// Build other directories to search (Current Directory, System directory, Windows Directory)
 
-		string final_path;
-		final_path.resize(MAX_PATH);
-		CopyMemory((PVOID)final_path.data(), name.c_str(), name.size());
-		
-		if (!PathFindOnPath((LPSTR)(final_path.data()), NULL)) {
+		vector<byte> current_dir(MAX_PATH, 0);
+		auto current_dir_path_length = GetCurrentDirectory(current_dir.size(), (LPSTR)current_dir.data());
+		current_dir.resize(current_dir_path_length + 1); // +1 for null termination
+
+		vector<byte> system_directory(MAX_PATH, 0);
+		auto system_directory_path_length = GetSystemDirectory((LPSTR)system_directory.data(), system_directory.size());
+		system_directory.resize(system_directory_path_length + 1); // +1 for null termination
+
+		vector<byte> windows_directory(MAX_PATH, 0);
+		auto windows_directory_path_length = GetWindowsDirectory((LPSTR)windows_directory.data(), windows_directory.size());
+		windows_directory.resize(windows_directory_path_length + 1); // +1 for null termination
+
+		// We are operating on a copy of the original name, and resize is guaranteed to keep the existing data, so this statement is cool
+		name.resize(MAX_PATH);
+
+		vector<LPSTR> other_dirs = { (LPSTR)current_dir.data(), (LPSTR)system_directory.data(), (LPSTR)windows_directory.data(), NULL };
+		if (!PathFindOnPath((LPSTR)(name.data()), (PZPCSTR)other_dirs.data())) {
 			TRACE_AND_THROW_WINAPI(PathFindOnPathFailedException, PathFindOnPath);
 		}
 
-		return final_path;
+		// Rebuild the string, since we don't want all the extra zeros, and finding the last one is a drag.
+		return string((const char *)(name.data()));
 	}
 
 	string DllLoader::get_uniform_dll_name(string name) {
--- PirateLoader/main.cpp
+++ PirateLoader/main.cpp
@@ -13,16 +13,30 @@ using namespace pirateloader::dllloader;
 typedef int(*FuncType)();
 int main(int argc, char *argv[]) {
 	try {
+		// Load by buffer
 		auto dll_buffer = get_dll_buffer(argv[1]);
 		display_pe_header_statistics(get_pe_header_pointer(dll_buffer));
 
 		auto m = DllLoader::get().load(dll_buffer);
 
-		// Now dll is loaded, lets find exports
-		auto exported_function = m->get_proc_address("Func");
-		((FuncType)(exported_function))();
-		exported_function = m->get_proc_address("Func3");
-		((FuncType)(exported_function))();
+		auto Func = m->get_proc_address("Func");
+		((FuncType)(Func))();
+		auto Func3 = m->get_proc_address("Func3");
+		((FuncType)(Func3))();
+
+
+		// Load by name
+		auto module_by_name = DllLoader::get().load("kernel32.dll");
+
+		auto e1 = module_by_name->get_proc_address("GetErrorMode");
+		//((FuncType)(e1))();
+		auto e2 = module_by_name->get_proc_address("SetErrorMode");
+		//((FuncType)(e2))();
+
+
+
+
+
 
 	}
 	catch (const CommonException&) {
